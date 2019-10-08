48f2532701725bd35c8ab40ab5227ef15b4305cc
singularitea <bvscott66@gmail.com>
2019-10-08 22:10:53 +0800

    updated sensor models and added to admin

--- sensorapi/sensor/admin.py
+++ sensorapi/sensor/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from sensor.models import SensorDetail, SensorReading
 
-# Register your models here.
+admin.site.register(SensorDetail)
+
+admin.site.register(SensorReading)
--- sensorapi/sensor/models.py
+++ sensorapi/sensor/models.py
@@ -1,24 +1,22 @@
 from django.db import models
 from taggit.managers import TaggableManager
 
-# Sensor types for grouping and mass changes
-class SensorType(models.Model):
-    type_name = models.CharField(max_length=70)
-
-# 
-class SensorDetails(models.Model):
+class SensorDetail(models.Model):
     sensor_name = models.CharField(max_length=70)
     latitude = models.DecimalField(max_digits=14, decimal_places=4)
     longitude = models.DecimalField(max_digits=14, decimal_places=4)
     easting = models.DecimalField(max_digits=14, decimal_places=4)
     northing = models.DecimalField(max_digits=14, decimal_places=4)
     elevation = models.DecimalField(max_digits=10, decimal_places=4)
+    install_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     tags = TaggableManager()
     def __str__(self):
         return str(self.sensor_name)
 
 class SensorReading(models.Model):
-    sensor_name = models.ForeignKey(SensorDetails, db_column='sensor_name', on_delete=models.CASCADE)
+    sensor_name = models.ForeignKey(SensorDetail, db_column='sensor_name', on_delete=models.CASCADE)
     reading_date = models.DateTimeField()
     reading = models.DecimalField(max_digits=14, decimal_places=4)
     def __str__(self):
